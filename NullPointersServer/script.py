bfe900f32a910c872bce2231f086f29fe69e7805
Ki3mON <maciej.kmak@op.pl>
2025-03-29 17:34:43 +0100

    dictfix

--- NullPointersServer/script.py
+++ NullPointersServer/script.py
@@ -1,5 +1,6 @@
 import sys
 import os
+from plot import create_analysis_chart 
 
 sys.path.append(os.path.abspath(".."))  # Dodaje folder nadrzędny do ścieżki
 
@@ -9,14 +10,14 @@ from Model.for_api.api import api
 args = sys.argv[1:]
 
 
-AVERAGE_CAR_BATTERY_CAPACITY = 60  # w kWh
+AVERAGE_CAR_BATTERY_CAPACITY = args["usageAmount"]  # w kWh
 
-more_cars = args["cars"]
-battery_capacity = args["battery_capacity"]  # w kWh
-battery_percentage = args["battery_percentage"] # w %
-average_office_daily_consumption = args["monthly_consumption"]//30  # w kWh
-charger_power = args["charger_power"]  # w kW
-solar_power = args["solar_power"]  # w kW
+more_cars = args["carAmount"] ## OPTIONAL
+battery_capacity = args["batteryCapacity"]  # w kWh
+battery_percentage = args["batteryPercentage"] # w %
+average_office_daily_consumption = args["monthlyUsage"]//30  # w kWh
+charger_power = args["sourcePower"]  # w kW
+solar_power = args["solorPower"]  # w kW
 remaining_energy = (battery_capacity * battery_percentage)
 required_energy = average_office_daily_consumption + AVERAGE_CAR_BATTERY_CAPACITY * more_cars - remaining_energy
 if required_energy > 0:
@@ -27,5 +28,6 @@ if args:
     print(args)
 
     print(api(required_energy, charger_power, solar_power))
+    create_analysis_chart(required_energy, charger_power, solar_power)
 else:
     print("Brak argumentów, zwracam domyślną wiadomość.")
