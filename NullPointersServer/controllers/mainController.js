1945de48b6351b7da82f6ed281144681809e6d78
Wiktor Onik <wiktor.onik@gmail.com>
2025-03-29 16:26:35 +0100

    liii arg przekazuje

--- NullPointersServer/controllers/mainController.js
+++ NullPointersServer/controllers/mainController.js
@@ -13,7 +13,13 @@ const test = (req, res) => {
 
 
     pythonProcess.stdout.on('data', (data) => {
+
+
+
         console.log(`Odpowiedź z Pythona: ${data}`);
+
+
+
     });
 
     pythonProcess.stderr.on('data', (data) => {
@@ -35,6 +41,44 @@ const downloadData = (req, res) => {
     data = req.body
     console.log(data)
     res.status(200).json({ message: 'Data downloaded' })
+
+
+
+    const { spawn } = require('child_process');
+
+
+    const args = Object.entries(data).flatMap(([key, value]) => [value]);
+
+
+    const pythonProcess = spawn('python3', ['-m', 'script', ...args]);
+
+
+
+    pythonProcess.stdout.on('data', (data) => {
+
+
+
+        console.log(`Odpowiedź z Pythona: ${data}`);
+
+    });
+
+    pythonProcess.stderr.on('data', (data) => {
+        console.error(`Błąd Pythona: ${data}`);
+    });
+
+    pythonProcess.on('close', (code) => {
+        console.log(`Proces zakończył się kodem: ${code}`);
+    });
+
+
+    console.log(data)
+
+
+
+
+
+
+
 }
 
 
