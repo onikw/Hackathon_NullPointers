8004b2491870fbdab57889902a0f62d3357eb57f
Kacperon <kacper.feliks.455@gmail.com>
2025-03-29 13:24:26 +0100

    api for testing

--- Model/for_api/api.py
+++ Model/for_api/api.py
@@ -1,5 +1,7 @@
-
+from calculate_heures import calculate_hours_from_day_time
+from random import randint
 
 def api(energy:int,power:int):
+    data=[randint(0,100) for i in range(24)]
 
-    return ((0,0),(24,0))
+    return calculate_hours_from_day_time(data, energy, power)
