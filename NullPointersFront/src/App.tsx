64590e284bbbc21c1ad225633cdf711162417152
PawoLin <czajczykp@gmail.com>
2025-03-29 17:34:38 +0100

    zaktualiozwanie formularze

--- NullPointersFront/src/App.tsx
+++ NullPointersFront/src/App.tsx
@@ -2,6 +2,7 @@ import FormAdditional from "./components/FormAdditional.tsx";
 import ResultComponent from "./components/ResultComponent.tsx";
 import HeaderComponent from "./components/HeaderComponent.tsx";
 import {FormProvider} from "./contexts/FormContext.tsx";
+import ImageComponent from "./components/ImageComponent.tsx";
 
 function App() {
   return (
@@ -17,12 +18,17 @@ function App() {
                 className="w-1/4 border h-fit [50%] shadow-xl shadow-gray-600 m-5 border-gray-300 rounded-lg p-4 bg-gray-700">
                 <FormAdditional/>
               </aside>
-              <section className={"p-4 bg-gray-800 border border-gray-600 rounded-lg h-fit m-5"}>
-                <ResultComponent/>
-              </section>
+              <div>
+                <section className={"p-4 bg-gray-800 border border-gray-600 rounded-lg h-fit m-5"}>
+                  <ResultComponent/>
+                </section>
+                <section className={"border"}>
+                  Godziny optymalnego ładowania banku energii
+                </section>
+              </div>
             </div>
             <main className="flex-grow p-4">
-
+              <ImageComponent/>
             </main>
           </div>
           <footer className="bg-gray-900 text-white p-4 text-center">
